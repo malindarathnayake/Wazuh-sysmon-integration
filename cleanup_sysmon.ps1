5d70ba42982ddbc7c172fbe00c5ee0abf4862d2b
Malinda <malinda.rathnayake@outlook.com>
2024-10-22 14:48:37 -0400

    Create cleanup_sysmon.ps1

---
+++ cleanup_sysmon.ps1
@@ -0,0 +1,10 @@
+cmd.exe /c "Sysmon64 -u force 2>&1"
+cmd /c "net stop Sysmon64 2>&1"
+cmd /c "net stop sysmondrv 2>&1"
+Remove-Item -Path "C:\Windows\Sysmon64.exe" -Force
+Remove-Item -Path "C:\Windows\SysmonDrv.sys" -Force
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SysmonDrv" -Recurse -Force
+Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Sysmon64" -Recurse -Force
+fltmc unload SysmonDrv
+cmd /c "sc delete sysmon64"
+cmd /c "sc delete sysmondrv"
